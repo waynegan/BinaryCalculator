47ff8da5d172a8e382db93a2e2afdf634c2aa0a5
Wei Gan <ganwei92@gmail.com>
2018-03-09 08:05:29 +0800

    resolve bugs for display

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,4 +1,7 @@
 var display = "";
+var hasEq = 0;
+var operatorBuffer = "";
+var isContainOperator = false;
 
 function eq() {
   if (display.indexOf("+") != -1) {
@@ -28,13 +31,41 @@ function eq() {
   }
   display = ans;
   document.getElementById("res").innerHTML = display;
+  hasEq = 1;
 }
 
 function clickButton(value){
+    if (hasEq == 1)
+    {
+      clr();
+    }
+    hasEq = 0; // auto clear from display
     display += value;
     document.getElementById("res").innerHTML = display;
+    operatorBuffer = null;
+}
+function clickOperator(value){
+  if( operatorBuffer !== null)
+  {
+    display.replace(/.$/,".") = '';  //update to new operator
+  }
+  if(isContainOperator){
+    ans = display;
+    eq();
+    hasEq = 0;
+  }
+  else{
+    isContainOperator = true;
+  }
+  
+  display += value;
+  document.getElementById("res").innerHTML = display;
+  operatorBuffer = value;
+  
+
 }
 function clr() {
   display = "";
   document.getElementById("res").innerHTML = display;
+  isContainOperator = false;
 };
